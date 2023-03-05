53aee6f0b39ea7c57e1cf433cddd87808626413f
Puran Lai <1696095803@qq.com>
2023-03-05 19:51:26 +0800

    添加项目文件。

---
+++ MainProgram/MainWindow.xaml.cs
@@ -0,0 +1,58 @@
+﻿using MainProgram.Windows;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MainProgram
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        PPTClassifier Classifier = new();
+        public MainWindow()
+        {
+            InitializeComponent();
+            Classifier.Show();
+            ClassifyToggleButton.IsOn = true;
+            if (ClassifyToggleButton.IsOn)
+            {
+                Classifier.Visibility = Visibility.Visible;
+                Dispatcher.BeginInvoke(() =>
+                {
+                    this.Visibility = Visibility.Collapsed;
+                });
+            }
+            else
+                Classifier.Visibility = Visibility.Collapsed;
+        }
+
+        private void ClassifyToggleButton_Toggled(object sender, RoutedEventArgs e)
+        {
+            if (ClassifyToggleButton.IsOn)
+            {
+                Classifier.Visibility = Visibility.Visible;
+            }
+            else
+                Classifier.Visibility = Visibility.Collapsed;
+        }
+
+        private void Window_Closed(object sender, EventArgs e)
+        {
+            App.Current.Shutdown();
+        }
+    }
+}
---
+++ MainProgram/Windows/ClassifyWindow.xaml.cs
@@ -0,0 +1,134 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Interop;
+using Microsoft.VisualBasic.FileIO;
+using System.Threading.Tasks;
+
+namespace MainProgram.Windows
+{
+    /// <summary>
+    /// ClassifyWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class PPTClassifier : Window
+    {
+        DateTime LastOpen;
+        bool DoubleClicked = false;
+        public PPTClassifier()
+        {
+            InitializeComponent();
+            Left = SystemParameters.PrimaryScreenWidth - Width;
+            Top = (SystemParameters.PrimaryScreenHeight - Height) / 5 * 2;
+        }
+
+        private void SelectFiles(string subject)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Title = "选择课件";
+            openFileDialog.Filter = "课件|*.*";
+            openFileDialog.FileName = string.Empty;
+            openFileDialog.FilterIndex = 1;
+            openFileDialog.Multiselect = true;
+            openFileDialog.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+            if (Directory.Exists("E:\\"))
+            {
+                openFileDialog.InitialDirectory = "E:\\";
+            }
+            if (openFileDialog.ShowDialog() == false)
+            {
+                return;
+            }
+            string[] files = openFileDialog.FileNames;
+            CopyFiles(subject, files);
+        }
+
+        private void GetDroppedFiles(string subject, DragEventArgs e)
+        {
+            try
+            {
+                string[] files = (string[])e.Data.GetData(DataFormats.FileDrop);
+                CopyFiles(subject, files);
+            }
+            catch { }
+        }
+
+        private void CopyFiles(string subject, string[] files)
+        {
+            string folder = $"D:/{subject}/";
+            if (!Directory.Exists(folder))
+            {
+                Directory.CreateDirectory(folder);
+            }
+            foreach (string file in files)
+            {
+                string destination = folder + Path.GetFileName(file);
+                if (destination == file)
+                {
+                    continue;
+                }
+                else if (File.Exists(destination))
+                {
+                    FileSystem.DeleteFile(destination, UIOption.OnlyErrorDialogs, RecycleOption.SendToRecycleBin);
+                }
+                File.Copy(file, destination);
+                if (Directory.GetParent(file).FullName ==
+                    Environment.GetFolderPath(Environment.SpecialFolder.DesktopDirectory))
+                {
+                    File.Delete(file);
+                }
+            }
+        }
+
+        private void DropButton_MouseRightButtonDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            SelectFiles(((Button)sender).Content.ToString());
+        }
+
+        private void DropButton_Drop(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                e.Effects = DragDropEffects.Copy;
+                GetDroppedFiles(((Button)sender).Content.ToString(), e);
+            }
+        }
+
+        private void Window_MouseEnter(object sender, System.Windows.Input.MouseEventArgs e)
+        {
+            OverTop.ExtentedWindowOps.ToBottom(this);
+        }
+
+        private async void DropButton_Click(object sender, RoutedEventArgs e)
+        {
+            WaitProgressRing.IsActive = true;
+            WaitProgressRing.Visibility = Visibility.Visible;
+            await Task.Delay(500);
+            if (DoubleClicked)
+            {
+                DoubleClicked = false;
+                return;
+            }
+
+            WaitProgressRing.IsActive = false;
+            WaitProgressRing.Visibility = Visibility.Collapsed;
+            if (DateTime.Now - LastOpen <= TimeSpan.FromMilliseconds(1500))
+            {
+                return;
+            }
+            Process.Start("explorer.exe", $"D:\\{((Button)sender).Content}");
+            LastOpen = DateTime.Now;
+        }
+
+        private void DropButton_MouseDoubleClick(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            DoubleClicked = true;
+            WaitProgressRing.IsActive = false;
+            WaitProgressRing.Visibility = Visibility.Collapsed;
+            SelectFiles(((Button)sender).Content.ToString());
+        }
+    }
+}
