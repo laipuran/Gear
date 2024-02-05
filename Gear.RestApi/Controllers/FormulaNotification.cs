8d2927416e5a0250920bc64e3a879e5b4a9c42db
Puran Lai <1696095803@qq.com>
2024-02-05 14:25:42 +0800

    Add: Same method for formulas

---
+++ Gear.RestApi/Controllers/FormulaNotification.cs
@@ -0,0 +1,42 @@
+﻿using Gear.Base.Class;
+using Gear.Base.Interface;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Gear.RestApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class FormulaNotification : ControllerBase
+    {
+        private readonly ILogger<FormulaNotification> _logger;
+        public static INotifyQueueService NotifyService { get; set; } = TextNotification.NotifyService;
+
+        public FormulaNotification(ILogger<FormulaNotification> logger)
+        {
+            _logger = logger;
+        }
+
+        private static INotifyQueueService GetQueueService()
+        {
+            var services = new ServiceCollection();
+            services.AddSingleton<INotifyQueueService, NotificationQueueService>();
+            var serviceProvider = services.BuildServiceProvider();
+#pragma warning disable CS8603 // 可能返回 null 引用。
+            return serviceProvider.GetService<INotifyQueueService>();
+#pragma warning restore CS8603 // 可能返回 null 引用。
+        }
+
+        [HttpGet(Name = "GetFormulas")]
+        public List<NotifyObject> Get()
+        {
+            return NotifyService.GetObjects(ContentForm.Formula);
+        }
+
+        [HttpPost(Name = "EnqueueFormula")]
+        public void Post(string content)
+        {
+            NotifyService.EnqueueNotification(new(ContentForm.Formula, content));
+
+        }
+    }
+}
